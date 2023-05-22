5039af0e7afaf5e7585f4f538f3c5820ebf235ff
Junye-Seo <134276039+Junye-Seo@users.noreply.github.com>
2023-05-23 07:11:42 +0900

    Update mylib.c - mult, div

--- mylib.c
+++ mylib.c
@@ -13,3 +13,15 @@ int add(int a, int b){
 int subtract(int a, int b){
 	return a - b;
 }
+
+int mult(int a, int b){
+	return a * b;
+}
+
+int div(int a, int b){
+	if(b == 0){
+		printf("divided-by-0 issue");
+		exit(-1);
+	}
+	return a / b;
+}
