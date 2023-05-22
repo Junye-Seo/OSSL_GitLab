325865b11b108010d7278a6fc94b81a2064f9636
Junye-Seo <134276039+Junye-Seo@users.noreply.github.com>
2023-05-23 07:08:54 +0900

    Update main.c - mult, div

--- main.c
+++ main.c
@@ -12,5 +12,8 @@ int main(void){
 	printf("(added) a + b = %d + %d = %d\n", a, b, add(a, b));
 	printf("(subtracted) a - b = %d - %d = %d\n", a, b, subtract(a, b));
 	
+	printf("(multiplied) a x b = %d x %d = %d\n", a, b, mult(a, b));
+	printf("(divided) a / b = %d / %d = %d\n", a, b, div(a, b));
+	
 	return 0;
 }
